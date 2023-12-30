c0a7bac262de7b560af7417f5a91dac9320a5cd0
Stephen Belyea <belyea.stephen@gmail.com>
2023-12-30 16:56:01 -0500

    Drop no-go weekend, add timeout loading for iframes and scroll on load

--- docs/search.js
+++ docs/search.js
@@ -1,6 +1,7 @@
 (() => {
   const searchForm = document.getElementById("search-parks");
   const searchResults = document.getElementById("search-results");
+  const submit = document.getElementById("submit");
 
   const ontarioParksBaseUrl =
     "https://reservations.ontarioparks.com/create-booking/results";
@@ -34,15 +35,52 @@
     return searchUrls;
   };
 
-  const onSubmitSearchParks = (event) => {
+  const createFrameNode = ({ searchUrl, message }) => {
+    const frame = document.createElement("iframe");
+    frame.setAttribute("src", searchUrl);
+    frame.setAttribute("title", "Search results for " + message);
+    frame.setAttribute("referrerpolicy", "same-origin");
+    frame.setAttribute(
+      "sandbox",
+      [
+        "allow-forms",
+        "allow-scripts",
+        "allow-downloads",
+        "allow-same-origin",
+        "allow-storage-access-by-user-activation",
+      ].join(" ")
+    );
+    return frame;
+  };
+
+  const createHeadingNode = ({ message }) => {
+    const heading = document.createElement("h2");
+    heading.textContent = message;
+    return heading;
+  };
+
+  const createArticleNode = (parkSearch) => {
+    const article = document.createElement("article");
+    const children = [
+      createHeadingNode(parkSearch),
+      createFrameNode(parkSearch),
+    ];
+    for (let c = 0; c < children.length; c++) {
+      article.appendChild(children[c]);
+    }
+    return article;
+  };
+
+  const onSubmitSearchParks = async (event) => {
     event.preventDefault();
+    submit.setAttribute("disabled", "true");
+    submit.textContent = "Loading...";
 
     const searchParks = [
       { park: "Pinery", campground: "Riverside Area 1", mapId: "-2147483326" },
     ];
     const searchDates = [
       { startDate: "2024-02-02", endDate: "2024-02-04" },
-      { startDate: "2024-02-09", endDate: "2024-02-11" },
       { startDate: "2024-02-16", endDate: "2024-02-18" },
       { startDate: "2024-02-23", endDate: "2024-02-25" },
     ];
@@ -50,20 +88,18 @@
     const allParksSearchUrls = getAllParksSearchUrls(searchParks, searchDates);
 
     for (let p = 0; p < allParksSearchUrls.length; p++) {
-      const parkSearch = allParksSearchUrls[p];
-
-      const searchFrame = document.createElement("iframe");
-      searchFrame.setAttribute("src", parkSearch.searchUrl);
-
-      const searchHeading = document.createElement("h2");
-      searchHeading.textContent = parkSearch.message;
-
-      const searchPanel = document.createElement("article");
-      searchPanel.appendChild(searchHeading);
-      searchPanel.appendChild(searchFrame);
-
-      searchResults.appendChild(searchPanel);
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          searchResults.appendChild(createArticleNode(allParksSearchUrls[p]));
+          resolve();
+        }, 250);
+      });
     }
+
+    submit.removeAttribute("disabled");
+    submit.textContent = "Search";
+
+    searchResults.scrollIntoView();
   };
 
   searchForm.addEventListener("submit", onSubmitSearchParks);
